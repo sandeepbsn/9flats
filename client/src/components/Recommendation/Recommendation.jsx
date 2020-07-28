61018f5203814564ccbb4bf9e9b691b3b465fa5d
brijkrpr22 <brijkrpr22@gmail.com>
2020-07-28 16:59:26 +0530

    recommendation component

---
+++ client/src/components/Recommendation/Recommendation.jsx
@@ -0,0 +1,267 @@
+import React from "react"
+import { ControlledCarousel, Carousel, Card, CardDeck, Container } from "react-bootstrap"
+
+
+
+
+function Recommendation() {
+    return (
+        <Container className="mt-5">
+            <Carousel>
+                <Carousel.Item>
+
+                    <CardDeck>
+                        <Card>
+                            <Card.Img variant="top" src="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" data-original-uri="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" />
+
+                            {/* <img class="_91slf2a" aria-hidden="true" alt="Panoramic Sea View Apartment At Dona Paula" loading="lazy" src="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" data-original-uri="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" style="object-fit: cover;"> */}
+
+                            <Card.Body>
+                                <Card.Text>
+                                    <span className="text-muted">
+                                        <small> Entire Apartment</small>
+                                        <small > . </small>
+
+                                        <small > 3Beds</small>
+
+                                    </span>
+
+                                    <div >
+                                        <h5>Panoramic Sea View Apartment At Dona Paula</h5></div>
+                                </Card.Text>
+                                <Card.Title>
+                                    <span>
+                                        <strong>$ 7,74.500</strong>
+                                          /month
+                                           </span></Card.Title>
+                            </Card.Body>
+
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" data-original-uri="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" />
+
+                            {/* <img class="_91slf2a" aria-hidden="true" alt="Panoramic Sea View Apartment At Dona Paula" loading="lazy" src="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" data-original-uri="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" style="object-fit: cover;"> */}
+
+                            <Card.Body>
+                                <Card.Text>
+                                    <span className="text-muted">
+                                        <small> Entire Apartment</small>
+                                        <small > . </small>
+
+                                        <small > 3Beds</small>
+
+                                    </span>
+
+                                    <div >
+                                        <h5>Panoramic Sea View Apartment At Dona Paula</h5></div>
+                                </Card.Text>
+                                <Card.Title>
+                                    <span>
+                                        <strong>$ 7,74.500</strong>
+                                          /month
+                                           </span></Card.Title>
+                            </Card.Body>
+
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" data-original-uri="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" />
+
+                            {/* <img class="_91slf2a" aria-hidden="true" alt="Panoramic Sea View Apartment At Dona Paula" loading="lazy" src="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" data-original-uri="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" style="object-fit: cover;"> */}
+
+                            <Card.Body>
+                                <Card.Text>
+                                    <span className="text-muted">
+                                        <small> Entire Apartment</small>
+                                        <small > . </small>
+
+                                        <small > 3Beds</small>
+
+                                    </span>
+
+                                    <div >
+                                        <h5>Panoramic Sea View Apartment At Dona Paula</h5></div>
+                                </Card.Text>
+                                <Card.Title>
+                                    <span>
+                                        <strong>$ 7,74.500</strong>
+                                          /month
+                                           </span></Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </CardDeck>
+                </Carousel.Item>
+
+
+                <Carousel.Item>
+
+                    <CardDeck>
+                        <Card>
+                            <Card.Img variant="top" src="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" data-original-uri="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" />
+
+                            {/* <img class="_91slf2a" aria-hidden="true" alt="Panoramic Sea View Apartment At Dona Paula" loading="lazy" src="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" data-original-uri="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" style="object-fit: cover;"> */}
+
+                            <Card.Body>
+                                <Card.Text>
+                                    <span className="text-muted">
+                                        <small> Entire Apartment</small>
+                                        <small > . </small>
+
+                                        <small > 3Beds</small>
+
+                                    </span>
+
+                                    <div >
+                                        <h5>Panoramic Sea View Apartment At Dona Paula</h5></div>
+                                </Card.Text>
+                                <Card.Title>
+                                    <span>
+                                        <strong>$ 7,74.500</strong>
+                      /month
+                       </span></Card.Title>
+                            </Card.Body>
+
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" data-original-uri="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" />
+
+                            {/* <img class="_91slf2a" aria-hidden="true" alt="Panoramic Sea View Apartment At Dona Paula" loading="lazy" src="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" data-original-uri="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" style="object-fit: cover;"> */}
+
+                            <Card.Body>
+                                <Card.Text>
+                                    <span className="text-muted">
+                                        <small> Entire Apartment</small>
+                                        <small > . </small>
+
+                                        <small > 3Beds</small>
+
+                                    </span>
+
+                                    <div >
+                                        <h5>Panoramic Sea View Apartment At Dona Paula</h5></div>
+                                </Card.Text>
+                                <Card.Title>
+                                    <span>
+                                        <strong>$ 7,74.500</strong>
+                      /month
+                       </span></Card.Title>
+                            </Card.Body>
+
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" data-original-uri="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" />
+
+                            {/* <img class="_91slf2a" aria-hidden="true" alt="Panoramic Sea View Apartment At Dona Paula" loading="lazy" src="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" data-original-uri="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" style="object-fit: cover;"> */}
+
+                            <Card.Body>
+                                <Card.Text>
+                                    <span className="text-muted">
+                                        <small> Entire Apartment</small>
+                                        <small > . </small>
+
+                                        <small > 3Beds</small>
+
+                                    </span>
+
+                                    <div >
+                                        <h5>Panoramic Sea View Apartment At Dona Paula</h5></div>
+                                </Card.Text>
+                                <Card.Title>
+                                    <span>
+                                        <strong>$ 7,74.500</strong>
+                      /month
+                       </span></Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </CardDeck>
+                </Carousel.Item>
+
+                <Carousel.Item>
+
+                    <CardDeck>
+                        <Card>
+                            <Card.Img variant="top" src="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" data-original-uri="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" />
+
+                            {/* <img class="_91slf2a" aria-hidden="true" alt="Panoramic Sea View Apartment At Dona Paula" loading="lazy" src="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" data-original-uri="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" style="object-fit: cover;"> */}
+
+                            <Card.Body>
+                                <Card.Text>
+                                    <span className="text-muted">
+                                        <small> Entire Apartment</small>
+                                        <small > . </small>
+
+                                        <small > 3Beds</small>
+
+                                    </span>
+
+                                    <div >
+                                        <h5>Panoramic Sea View Apartment At Dona Paula</h5></div>
+                                </Card.Text>
+                                <Card.Title>
+                                    <span>
+                                        <strong>$ 7,74.500</strong>
+                      /month
+                       </span></Card.Title>
+                            </Card.Body>
+
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" data-original-uri="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" />
+
+                            {/* <img class="_91slf2a" aria-hidden="true" alt="Panoramic Sea View Apartment At Dona Paula" loading="lazy" src="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" data-original-uri="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" style="object-fit: cover;"> */}
+
+                            <Card.Body>
+                                <Card.Text>
+                                    <span className="text-muted">
+                                        <small> Entire Apartment</small>
+                                        <small > . </small>
+
+                                        <small > 3Beds</small>
+
+                                    </span>
+
+                                    <div >
+                                        <h5>Panoramic Sea View Apartment At Dona Paula</h5></div>
+                                </Card.Text>
+                                <Card.Title>
+                                    <span>
+                                        <strong>$ 7,74.500</strong>
+                      /month
+                       </span></Card.Title>
+                            </Card.Body>
+
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" data-original-uri="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" />
+
+                            {/* <img class="_91slf2a" aria-hidden="true" alt="Panoramic Sea View Apartment At Dona Paula" loading="lazy" src="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" data-original-uri="https://a0.muscache.com/im/pictures/11f5e49c-3009-4118-8a60-cf1377d19d16.jpg?aki_policy=large" style="object-fit: cover;"> */}
+
+                            <Card.Body>
+                                <Card.Text>
+                                    <span className="text-muted">
+                                        <small> Entire Apartment</small>
+                                        <small > . </small>
+
+                                        <small > 3Beds</small>
+
+                                    </span>
+
+                                    <div >
+                                        <h5>Panoramic Sea View Apartment At Dona Paula</h5></div>
+                                </Card.Text>
+                                <Card.Title>
+                                    <span>
+                                        <strong>$ 7,74.500</strong>
+                      /month
+                       </span></Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </CardDeck>
+                </Carousel.Item>
+
+            </Carousel>
+
+        </Container>
+    )
+}
+
+export default Recommendation
