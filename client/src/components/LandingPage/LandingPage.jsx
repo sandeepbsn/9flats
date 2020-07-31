93c96ff269e9b85203b660d9afce02e7ab9770a7
Krush159 <krushna159@gmail.com>
2020-08-01 02:41:22 +0530

    topCities & datepicker css Updated

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,30 +1,30 @@
-import React, {useState, useEffect} from "react";
-import {useSelector, useDispatch} from 'react-redux'
-import {useHistory} from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import styles from './LandingPage.module.css'
 import DatePicker from "react-datepicker";
 import TopCities from './TopCities'
 import PopularPlaces from './PopularPlaces'
-import {clearValidation} from '../../redux/actions/entityActions'
+import { clearValidation } from '../../redux/actions/entityActions'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
 
 
 
 
-export default function LandingPage(){
+export default function LandingPage() {
   const final_res = useSelector(state => state.entity.validation)
   const dispatch = useDispatch()
   const [startDate, setStartDate] = useState(new Date());
 
   const copy = new Date(Number(startDate))
-  copy.setDate(startDate.getDate()+1)
+  copy.setDate(startDate.getDate() + 1)
 
   const [endDate, setEndDate] = useState(copy);
 
   const [params, setParams] = useState({
-    "search_query":"",
-    "guests":""
+    "search_query": "",
+    "guests": ""
   })
   const loginInfo = useSelector(state => state.login.loginInfo)
   const history = useHistory()
@@ -33,123 +33,124 @@ export default function LandingPage(){
     console.log(e.target.name)
     setParams({
       ...params,
-      [e.target.name]:e.target.value
+      [e.target.name]: e.target.value
     })
   }
 
-  useEffect(()=>{
-    if(final_res?.['status']){
+  useEffect(() => {
+    if (final_res?.['status']) {
       dispatch(clearValidation())
     }
     let end_update = new Date(Number(startDate))
-    end_update.setDate(startDate.getDate()+1)
+    end_update.setDate(startDate.getDate() + 1)
     setEndDate(end_update)
-  },[startDate])
+  }, [startDate])
 
   const handleSearch = (e) => {
     e.preventDefault()
     console.log(params, startDate, endDate)
     let start = startDate.toLocaleDateString().split("/").reverse().join("-")
     let end = endDate.toLocaleDateString().split("/").reverse().join("-")
-    let {search_query} = params
+    let { search_query } = params
     history.push(`/listing?search_query=${search_query}&start_date=${start}&end_date=${end}&page=&per_page=&category=&amenities=&price=`)
   }
-  
+
   return (
     <>
-        <div className="container-fluid" >
-          <div className={`${styles.landingImage} row`}>
-            <div className={`${styles.over} container-fluid`} style={{height:400}}>
-              <div className="row text-center my-5" >
-                <h3 className="col-12 text-white " style ={{marginTop:50}}>
-                  <b>Feel like Home away from Home</b>
-                </h3>
-                <h5 className="col-12 text-white">
-                  <b>The alternative to hotel with 6 million homes worldwide</b>
-                </h5>
-              </div>
-              <div className="container searchPanel mb-5" >
-                <div className="row m-5  justify-content-center">
-                  <div className={`${styles.searchbg} rounded p-2`}>
-                    <form onSubmit={(e)=>handleSearch(e)} className="d-flex justify-content-center">
-                      
-                      <GooglePlacesAutocomplete
-                        types = "region"
-                        onSelect={(description)=>setParams({
-                          ...params,
-                          "search_query":description['description']
-                        })}
-                        // apiKey="AIzaSyCcS0j7hDpSs-F4xDi2q6AkTD_sWqECR9M"
-                        renderInput={(props) => (
-                          <div>
-                            <input className={styles.wheretogo}
-                              // Custom properties
-                              {...props}
-                            />
-                          </div>
-                        )}
-                        renderSuggestions={(active, suggestions, onSelectSuggestion) => (
-                          <div className={styles.suggestionsContainer}>
-                            {
-                              suggestions.map((suggestion) => (
-                                <div
-                                  className={styles.suggestion}
-                                  onClick={(event) => onSelectSuggestion(suggestion, event)}
-                                >
-                                  {suggestion.description}
-                                </div>
-                              ))
-                            }
-                          </div>
-                        )}
-                      />                         
-                      <div>
-                      <DatePicker
-                        className={`${styles.datecomponent} ${styles.fromDate} mx-2`}
-                        selected={startDate}
-                        onChange={date => setStartDate(date)}
-                        selectsStart
-                        startDate={startDate}
-                        endDate={endDate}
-                        minDate={startDate}
-                        placeholderText="From"
-                      />
-                      <DatePicker
-                        className={`${styles.datecomponent} ${styles.toDate} mx-2`}
-                        selected={endDate}
-                        onChange={date => setEndDate(date)}
-                        selectsEnd
-                        startDate={startDate}
-                        endDate={endDate}
-                        minDate={endDate}
-                        placeholderText="To"
-                      />
-                      </div>
-                      <input
-                        type="text"
-                        name="guests"
-                        id="search_number_of_beds"
-                        placeholder="Guests"
-                        tabIndex="4"
-                        value={params['guests']}
-                        onChange={(e)=>handleChange(e)}
-                        className={`${styles.beds} mr-sm-2`} />
-                        <button
-                          type="submit"
-                          name="commit"
-                          className={`${styles.searchSubmit}`}
-                          id="fullSearch">Search
-                        </button>
-                    </form>
-                  </div>
-                </div>
+      <div className="container-fluid" >
+        <div className={`${styles.landingImage} row`}>
+          <div className={`${styles.over} container-fluid`} style={{ height: 400 }}>
+            <div className="row text-center my-5 d-none  d-md-block" >
+              <h2 className="col-12 text-white " style={{ marginTop: 80 }}>
+                <b>Feel like Home away from Home</b>
+              </h2>
+              <h6 className="col-12 text-white mt-4 mb-4">
+                The alternative to hotel with 6 million homes worldwide
+              </h6>
+            </div>
+            <div className="container searchPanel  my-5" >
+              <div className="m-auto text-center">
+                <form 
+                  onSubmit={(e) => handleSearch(e)} 
+                  className={`${styles.searchbg} rounded p-2 d-flex flex-md-row flex-column d-md-inline-flex justify-content-around`}>
+                    <GooglePlacesAutocomplete
+                      types="region"
+                      onSelect={(description) => setParams({
+                        ...params,
+                        "search_query": description['description']
+                      })}
+                      // apiKey="AIzaSyCcS0j7hDpSs-F4xDi2q6AkTD_sWqECR9M"
+                      renderInput={(props) => (
+                        <div>
+                          <input className={`${styles.wheretogo}`}
+                            // Custom properties
+                            {...props}
+                          />
+                        </div>
+                      )}
+                      renderSuggestions={(active, suggestions, onSelectSuggestion) => (
+                        <div className={styles.suggestionsContainer}>
+                          {
+                            suggestions.map((suggestion) => (
+                              <div
+                                className={styles.suggestion}
+                                onClick={(event) => onSelectSuggestion(suggestion, event)}
+                              >
+                                {suggestion.description}
+                              </div>
+                            ))
+                          }
+                        </div>
+                      )}
+                    />
+                  <DatePicker
+                    className={`${styles.datecomponent} ${styles.fromDate} mx-md-2`}
+                    selected={startDate}
+                    onChange={date => setStartDate(date)}
+                    selectsStart
+                    startDate={startDate}
+                    endDate={endDate}
+                    minDate={startDate}
+                    placeholderText="From"
+                  />
+
+
+                  <DatePicker
+                    className={`${styles.datecomponent} ${styles.toDate} `}
+                    selected={endDate}
+                    onChange={date => setEndDate(date)}
+                    selectsEnd
+                    startDate={startDate}
+                    endDate={endDate}
+                    minDate={endDate}
+                    placeholderText="To"
+                  />
+                  <input
+                    className={`${styles.beds} mx-md-2`}
+                    type="text"
+                    name="guests"
+                    id="search_number_of_beds"
+                    placeholder="Guests"
+                    tabIndex="4"
+                    value={params['guests']}
+                    onChange={(e) => handleChange(e)}
+                  />
+
+                  <button
+                    type="submit"
+                    name="commit"
+                    className={`${styles.searchSubmit}`}
+                    id="fullSearch">Search
+                    </button>
+                </form>
               </div>
             </div>
           </div>
-          <TopCities/>
-          <PopularPlaces/> 
         </div>
-      </>
+        <TopCities />
+        <PopularPlaces />
+      </div>
+    </>
   )
 }
 
--- client/src/components/LandingPage/TopCities.jsx
+++ client/src/components/LandingPage/TopCities.jsx
@@ -5,94 +5,177 @@ import styles from './LandingPage.module.css'
 function TopCities() {
     return (
         <div className="container top-cities mb-5 mt-3" >
-            <h2 className="mb-3">Our Top Cities</h2>
-            <CardDeck>
-                <Card className="rounded-border">
-                    <Card.Img variant="top"
-                        src="https://images.9flats.com/geo_locator_photos/photos/15-1552991527/small.jpg"
-                    />
-                    <Card.Body>
-                        <Card.Title className="text-primary">Berlin</Card.Title>
-                    </Card.Body>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="https://images.9flats.com/geo_locator_photos/photos/19-1552991863/small.jpg"
-                    />
-                    <Card.Body>
-                        <Card.Title className="text-primary">Amsterdam</Card.Title>
-                    </Card.Body>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="https://images.9flats.com/geo_locator_photos/photos/12-1552991296/small.jpg" />
-                    <Card.Body>
-                        <Card.Title className="text-primary">London</Card.Title>
-                    </Card.Body>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="https://images.9flats.com/geo_locator_photos/photos/23-1552992205/small.jpg" />
-                    <Card.Body>
-                        <Card.Title className="text-primary">Brussels</Card.Title>
-                    </Card.Body>
-                </Card>
-            </CardDeck>
-            <CardDeck className="mt-3">
-                <Card>
-                    <Card.Img variant="top"
-                        src="https://images.9flats.com/geo_locator_photos/photos/7-1552992258/small.jpg"
-                    />
-                    <Card.Body>
-                        <Card.Title className="text-primary h4 px-2">
-                            <a href="/berlin">Dubai</a>
-                        </Card.Title>
-                    </Card.Body>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="https://images.9flats.com/geo_locator_photos/photos/20-1552991880/small.jpg" />
-                    <Card.Body>
-                        <Card.Title className="text-primary">Barcelona</Card.Title>
-                    </Card.Body>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="https://images.9flats.com/geo_locator_photos/photos/17-1552991676/small.jpg" />
-                    <Card.Body>
-                        <Card.Title className="text-primary">Munich</Card.Title>
-                    </Card.Body>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="https://images.9flats.com/geo_locator_photos/photos/14-1552991516/small.jpg" />
-                    <Card.Body>
-                        <Card.Title className="text-primary">Paris</Card.Title>
-                    </Card.Body>
-                </Card>
-            </CardDeck>
-            <CardDeck className="mt-3">
-                <Card>
-                    <Card.Img variant="top"
-                        src="https://images.9flats.com/geo_locator_photos/photos/21-1552991908/small.jpg"
-                    />
-                    <Card.Body>
-                        <Card.Title className="text-primary">Madrid</Card.Title>
-                    </Card.Body>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="https://images.9flats.com/geo_locator_photos/photos/25-1552992428/small.jpg" />
-                    <Card.Body>
-                        <Card.Title className="text-primary">New York City</Card.Title>
-                    </Card.Body>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="https://images.9flats.com/geo_locator_photos/photos/13-1552991353/small.jpg" />
-                    <Card.Body>
-                        <Card.Title className="text-primary">Vienna</Card.Title>
-                    </Card.Body>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="https://images.9flats.com/geo_locator_photos/photos/56-1552992409/small.jpg" />
-                    <Card.Body>
-                        <Card.Title className="text-primary">Miami</Card.Title>
-                    </Card.Body>
-                </Card>
-            </CardDeck>
+            <h2 className="mb-3">Our Top-Cities</h2>
+            <div className="row">
+                <div className={`col-6 col-md-3 mb-4 ${styles.topCity}`} id="tc-berlin-germany" role="tabpanel">
+                    <div className="rounded border z-depth-1 overflow-hidden">
+                        <a href="/berlin-germany">
+                            <div className={`${styles.image}`}
+                                data-bg="url(https://images.9flats.com/geo_locator_photos/photos/15-1552991527/small.jpg)"
+                                data-was-processed="true"
+                                style={{ backgroundImage: "url(" + "https://images.9flats.com/geo_locator_photos/photos/15-1552991527/small.jpg" + ")" }}>
+                            </div>
+                        </a>
+                        <h2 className="h4 px-2">
+                            <a title="Berlin" href="/berlin-germany">Berlin</a>
+                        </h2>
+                    </div>
+                </div>
+                <div className={`col-6 col-md-3 mb-4 ${styles.topCity}`} id="tc-amsterdam-netherlands" role="tabpanel">
+                    <div className="rounded border z-depth-1 overflow-hidden">
+                        <a href="/amsterdam-netherlands">
+                            <div className={`${styles.image}`}
+                                data-bg="url(https://images.9flats.com/geo_locator_photos/photos/19-1552991863/small.jpg)"
+                                data-was-processed="true"
+                                style={{ backgroundImage: `url("https://images.9flats.com/geo_locator_photos/photos/19-1552991863/small.jpg")` }}>
+                            </div>
+                        </a>
+                        <h2 className="h4 px-2">
+                            <a title="Amsterdam" href="/amsterdam-netherlands">Amsterdam</a>
+                        </h2>
+                    </div>
+                </div>
+                <div className={`col-6 col-md-3 mb-4 ${styles.topCity}`} id="tc-london-united_kingdom" role="tabpanel">
+                    <div className="rounded border z-depth-1 overflow-hidden">
+                        <a href="/london-united_kingdom">
+                            <div className={`${styles.image}`}
+                                data-bg="url(https://images.9flats.com/geo_locator_photos/photos/12-1552991296/small.jpg)"
+                                data-was-processed="true"
+                                style={{ backgroundImage: `url(https://images.9flats.com/geo_locator_photos/photos/12-1552991296/small.jpg)` }}>
+                            </div>
+                        </a>
+                        <h2 className="h4 px-2">
+                            <a title="London" href="/london-united_kingdom">London</a>
+                        </h2>
+                    </div>
+                </div>
+                <div className={`col-6 col-md-3 mb-4 ${styles.topCity}`} id="tc-brussels-belgium" role="tabpanel">
+                    <div className="rounded border z-depth-1 overflow-hidden">
+                        <a href="/brussels-belgium">
+                            <div className={`${styles.image}`}
+                                data-bg="url(https://images.9flats.com/geo_locator_photos/photos/23-1552992205/small.jpg)"
+                                data-was-processed="true"
+                                style={{ backgroundImage: `url(https://images.9flats.com/geo_locator_photos/photos/23-1552992205/small.jpg)` }}>
+                            </div>
+                        </a>
+                        <h2 className="h4 px-2">
+                            <a title="Brussels" href="/brussels-belgium">Brussels</a>
+                        </h2>
+                    </div>
+                </div>
+                <div className={`col-6 col-md-3 mb-4 ${styles.topCity}`} id="tc-dubai-united_arab_emirates" role="tabpanel">
+                    <div className="rounded border z-depth-1 overflow-hidden">
+                        <a href="/dubai-united_arab_emirates">
+                            <div className={`${styles.image}`}
+                                data-bg="url(https://images.9flats.com/geo_locator_photos/photos/7-1552992258/small.jpg)"
+                                data-was-processed="true"
+                                style={{ backgroundImage: `url("https://images.9flats.com/geo_locator_photos/photos/7-1552992258/small.jpg")` }}>
+                            </div>
+                        </a>
+                        <h2 className="h4 px-2">
+                            <a title="Dubai" href="/dubai-united_arab_emirates">Dubai</a>
+                        </h2>
+                    </div>
+                </div>
+                <div className={`col-6 col-md-3 mb-4 ${styles.topCity}`} id="tc-barcelona-spain" role="tabpanel">
+                    <div className="rounded border z-depth-1 overflow-hidden">
+                        <a href="/barcelona-spain">
+                            <div className={`${styles.image}`}
+                                data-bg="url(https://images.9flats.com/geo_locator_photos/photos/20-1552991880/small.jpg)"
+                                data-was-processed="true"
+                                style={{ backgroundImage: `url(https://images.9flats.com/geo_locator_photos/photos/20-1552991880/small.jpg)` }}>
+                            </div>
+                        </a>
+                        <h2 className="h4 px-2">
+                            <a title="Barcelona" href="/barcelona-spain">Barcelona</a>
+                        </h2>
+                    </div>
+                </div>
+                <div className={`col-6 col-md-3 mb-4 ${styles.topCity}`} id="tc-munich-germany" role="tabpanel">
+                    <div className="rounded border z-depth-1 overflow-hidden">
+                        <a href="/munich-germany">
+                            <div className={`${styles.image}`}
+                                data-bg="url(https://images.9flats.com/geo_locator_photos/photos/17-1552991676/small.jpg)"
+                                data-was-processed="true"
+                                style={{ backgroundImage: `url("https://images.9flats.com/geo_locator_photos/photos/17-1552991676/small.jpg")` }}>
+                            </div
+                            ></a>
+                        <h2 className="h4 px-2">
+                            <a title="Munich" href="/munich-germany">Munich</a>
+                        </h2>
+                    </div>
+                </div>
+                <div className={`col-6 col-md-3 mb-4 ${styles.topCity}`} id="tc-paris-france" role="tabpanel">
+                    <div className="rounded border z-depth-1 overflow-hidden">
+                        <a href="/paris-france">
+                            <div className={`${styles.image}`}
+                                data-bg="url(https://images.9flats.com/geo_locator_photos/photos/14-1552991516/small.jpg)"
+                                data-was-processed="true"
+                                style={{ backgroundImage: `url("https://images.9flats.com/geo_locator_photos/photos/14-1552991516/small.jpg` }}>
+                            </div>
+                        </a>
+                        <h2 className="h4 px-2">
+                            <a title="Paris" href="/paris-france">Paris</a>
+                        </h2>
+                    </div>
+                </div>
+                <div className={`col-6 col-md-3 mb-4 ${styles.topCity}`} id="tc-madrid-spain" role="tabpanel">
+                    <div className="rounded border z-depth-1 overflow-hidden">
+                        <a href="/madrid-spain">
+                            <div className={`${styles.image}`}
+                                data-bg="url(https://images.9flats.com/geo_locator_photos/photos/21-1552991908/small.jpg)"
+                                data-was-processed="true"
+                                style={{ backgroundImage: `url("https://images.9flats.com/geo_locator_photos/photos/21-1552991908/small.jpg")` }}>
+                            </div>
+                        </a>
+                        <h2 className="h4 px-2">
+                            <a title="Madrid" href="/madrid-spain">Madrid</a>
+                        </h2>
+                    </div>
+                </div>
+                <div className={`col-6 col-md-3 mb-4 ${styles.topCity}`} id="tc-new_york_city-new_york" role="tabpanel">
+                    <div className="rounded border z-depth-1 overflow-hidden">
+                        <a href="/new_york_city-new_york">
+                            <div className={`${styles.image}`}
+                                data-bg="url(https://images.9flats.com/geo_locator_photos/photos/25-1552992428/small.jpg)"
+                                data-was-processed="true"
+                                style={{ backgroundImage: `url("https://images.9flats.com/geo_locator_photos/photos/25-1552992428/small.jpg")` }}>
+                            </div>
+                        </a>
+                        <h2 className="h4 px-2">
+                            <a title="New York City" href="/new_york_city-new_york">New York City</a>
+                        </h2>
+                    </div>
+                </div>
+                <div className={`col-6 col-md-3 mb-4 ${styles.topCity}`} id="tc-vienna-austria" role="tabpanel">
+                    <div className="rounded border z-depth-1 overflow-hidden">
+                        <a href="/vienna-austria">
+                            <div className={`${styles.image}`}
+                                data-bg="url(https://images.9flats.com/geo_locator_photos/photos/13-1552991353/small.jpg)"
+                                data-was-processed="true"
+                                style={{ backgroundImage: `url("https://images.9flats.com/geo_locator_photos/photos/13-1552991353/small.jpg")` }}>
+                            </div>
+                        </a>
+                        <h2 className="h4 px-2">
+                            <a title="Vienna" href="/vienna-austria">Vienna</a>
+                        </h2>
+                    </div>
+                </div>
+                <div className={`col-6 col-md-3 mb-4 ${styles.topCity}`} id="tc-miami-florida" role="tabpanel">
+                    <div className="rounded border z-depth-1 overflow-hidden">
+                        <a href="/miami-florida">
+                            <div className={`${styles.image}`}
+                                data-bg="url(https://images.9flats.com/geo_locator_photos/photos/56-1552992409/small.jpg)"
+                                data-was-processed="true"
+                                style={{ backgroundImage: `url("https://images.9flats.com/geo_locator_photos/photos/56-1552992409/small.jpg")` }}>
+                            </div>
+                        </a>
+                        <h2 className="h4 px-2">
+                            <a title="Miami" href="/miami-florida">Miami</a>
+                        </h2>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
