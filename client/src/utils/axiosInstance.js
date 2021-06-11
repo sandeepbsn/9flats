faeac0f42be0a1b9c9a4e9a4bea5ad5b5b590c7f
Sandeep Babu <sandeep.bsn@gmail.com>
2021-06-12 01:56:33 +0530

    feat: updated endpoints

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5000"
+  baseURL: "https://flats9.herokuapp.com"
 });
 
 axiosInstance.interceptors.request.use(request => {
