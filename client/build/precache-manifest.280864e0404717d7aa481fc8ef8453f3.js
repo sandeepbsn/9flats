24f9d99229ff8ba17316872eb4cd8944b97bb455
Sandeep Babu <sandeep.bsn@gmail.com>
2021-06-12 02:03:27 +0530

    feat: added build files

---
+++ client/build/precache-manifest.280864e0404717d7aa481fc8ef8453f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cef455b10c85e115dc45f5c66d6dda8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be1911946b61813755e5",
+    "url": "/static/css/2.15a55c61.chunk.css"
+  },
+  {
+    "revision": "77ac8fdb13d843d04291",
+    "url": "/static/css/main.f5ac9c04.chunk.css"
+  },
+  {
+    "revision": "be1911946b61813755e5",
+    "url": "/static/js/2.e185397a.chunk.js"
+  },
+  {
+    "revision": "f48657291363d0706160a4cab7e6d5fd",
+    "url": "/static/js/2.e185397a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77ac8fdb13d843d04291",
+    "url": "/static/js/main.7622a801.chunk.js"
+  },
+  {
+    "revision": "0d00637615ecbba2c23e",
+    "url": "/static/js/runtime-main.c9f3c4da.js"
+  }
+]);
--- server/instance/config.py
+++ server/instance/config.py
@@ -1,3 +1,3 @@
 SECRET_KEY='masai school'
-SQLALCHEMY_DATABASE_URI='mysql://root:Findingmyself_9596@localhost/9flats'
+SQLALCHEMY_DATABASE_URI='mysql://yr1t6507s3ki3mv1:vdf9qvd4ms3tna10@dfkpczjgmpvkugnb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/dpbe93rgvkzhj783'
 
